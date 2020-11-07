67ff149433e687ccfcfd3a1bbc593d2327e9e22e
MarcinMG-web <gierada.marcin@gmail.com>
2020-11-07 20:29:05 +0100

    Fixed navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -3,30 +3,37 @@ import {NavLink} from 'react-router-dom'
 
 const Navbar = () => {
     return (
-    
-    <nav className = "navbar navbar-expand-lg navbar-dark bg-dark" >
-        <div className = "container">
-                <NavLink className="navbar-brand" to="#">Single Page Aplication</NavLink>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
 
-                    <div className = "collapse navbar-collapse" >
-                        <ul className = "navbar-nav mr-auto" >
-                            <li className = "nav-item" >
-                                <NavLink className = "nav-link " exact to = "/">
-                                    Home 
-                                </NavLink>
-                            </li>
+    <nav className = "navbar navbar-expand-lg navbar-dark bg-dark"  >
+        <div className = "container">
+            <NavLink className="navbar-brand" to="#">Single Page Aplication</NavLink>
+                <button 
+                    className="navbar-toggler" 
+                    type="button" 
+                    data-toggle="collapse" 
+                    data-target="#navbarSupportedContent" 
+                    aria-controls="navbarToggleExternalContent" 
+                    aria-expanded="false" 
+                    aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+            
+                <div className = "collapse navbar-collapse" >
+                    <ul className = "navbar-nav mr-auto" >
+                        <li className = "nav-item activ" >
+                           <NavLink className = "nav-link " exact to = "/">
+                                Home 
+                            </NavLink>
+                        </li>
                             <li className="nav-item">
-                                <NavLink  className = "nav-link" exact to = "/about" >
-                                    About
-                                </NavLink>
-                            </li>
-                        </ul>
+                            <NavLink  className = "nav-link" exact to = "/about" >
+                                About
+                            </NavLink>
+                        </li>
+                    </ul>
                         
-                    </div>
-            </div>
+                </div>
+        </div>
     </nav>
 )}
 
